2a4a2d6d5923d54a7d98d85aa1a2daa012f4ab9f
Leticia Amorim <leticiaamorimd@icloud.com>
2022-03-01 13:10:06 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+# imports
+from asyncio.base_events import Server
+import dash
+from dash import  html
+
+
+# setup
+app =  dash.Dash(__name__, title='My First Dash Deployment')
+server = app.server
+
+# Layout
+app.layout = html.Div([
+    html.H3('My First Deployment!!!', style={
+        'color': 'aliceblue',
+        'padding-top': '50px',
+        'text-align': 'center',
+        'text-style': 'bold'
+    }),
+    html.P('Congratulations, you have successfully deployed your first dash app', style={
+        'color': 'red',
+        'text-align': 'center',
+        'padding-top': '50px'
+    })
+])
+
+
+
+# run: ensures we dont try to run the app using both gunicorn and flask's in-built server
+if __name__ == '__main__':
+    app.run_server(debug=True)
