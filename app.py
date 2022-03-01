800c053da5478186f25e9b9fcbea50dc82ff5b15
Leticia Amorim <leticiaamorimd@icloud.com>
2022-03-01 23:10:22 +0000

    Adding files

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 # imports
 from asyncio.base_events import Server
 import dash
-from dash import  html
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+#from dash import dcc
+
 
 
 # setup
-app =  dash.Dash(__name__, title='My First Dash Deployment')
+app = dash.Dash('', title='Consumer Behaviour', external_stylesheets=['static/photon.min.css'])
 server = app.server
+
+#------ import data
+# Region 
+#nyorkshire = pd.read_csv("nyorkshire_updated.csv") - when i uncomment it complains 
 
 # Layout
 app.layout = html.Div([
-    html.H1('Consumer Behaviour', style={
-        'color': 'black',
-        'padding-top': '50px',
-        'text-align': 'center',
-    }),
-    html.P('Congratulations, you have successfully deployed your first dash app', style={
-        'color': 'red',
-        'text-align': 'center',
-        'padding-top': '50px'
-    })
-])
+    html.H1('Consumer Behaviour'),
+    html.Div([
+        html.H4('5 most sold products'),
+        dcc.Graph(id='pie-graph',
+        figure={}, className='top_5'),
+
+        html.H4('5 least sold products')
+
+    ])
+    ])
 
 
 
