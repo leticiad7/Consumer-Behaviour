4bfe326f03071d13188c50ac00ce0dbab051776b
Leticia Amorim <leticiaamorimd@icloud.com>
2022-03-08 22:47:03 +0000

    Adding products most sold by branches

--- app.py
+++ app.py
@@ -7,19 +7,22 @@ import plotly.express as px
 
 #Quantity sold by all branches 
 quantity_sold_allbranches = pd.read_csv("data/allbranches_quantity_sold.csv")
-print(quantity_sold_allbranches)
+#print(quantity_sold_allbranches)
 quantity_sold_allbranches = px.bar(quantity_sold_allbranches, x="Branch", y = "Quantity sold", title = "Quantity sold by all branches")
 
 #Amount in GBP sold by all branches 
 amount_in_gbp_allbranches = pd.read_csv("data/allbranches_amountgbp.csv")
-print(amount_in_gbp_allbranches)
+#print(amount_in_gbp_allbranches)
 amount_in_gbp_allbranches = px.bar(amount_in_gbp_allbranches, x="Branch", y = "Amount Sold", title = "Amount in GBP sold by all branches")
 
 #Profitability
 profitability = pd.read_csv("data/profitability.csv")
-print(profitability)
+#print(profitability)
 profitability = px.bar(profitability, x="Branch", y = "Profitability", title = "Profitability")
 
+#
+belfast = pd.read_csv("data/belfast_fixed.csv")
+belfast = px.bar(belfast, x="products", y = "quantity")
 
 app = dash.Dash(__name__, title= "Dashboard")
 server = app.server
@@ -28,8 +31,14 @@ app.layout = html.Div([
     html.Div([
         html.H1("Consumer Behaviour"),
     ]),
-    
-    html.Div([
+      html.Div([
+        html.H2("Quantity sold by Belfast branch"),
+        html.Div([
+            dcc.Graph(figure= belfast)                        
+            ]),
+        ]),
+
+      html.Div([
         html.H2("Quantity sold by all branches"),
         html.Div([
             dcc.Graph(figure= quantity_sold_allbranches)                        
@@ -59,7 +68,14 @@ app.layout = html.Div([
 )               
 def plot_quantity_sold_allbranches(value):
     figure_one = px.bar(quantity_sold_allbranches[quantity_sold_allbranches["Branch"] == value], xlabel= "Branch", y = "Quantity sold")
-    #figure_one = px.bar(quantity_sold_allbranches[quantity_sold_allbranches["Branch"] == value], xlabel= "Branch", y = "Quantity sold")
+    return figure_one
+
+@app.callback(
+    Output(component_id="belfast", component_property="figure"),
+    Input(component_id="belfast", component_property="value")
+)               
+def plot_products_by_region(value):
+    figure_one = px.bar(belfast[belfast["products"] == value], xlabel= "products", y = "quantity")
     return figure_one
 
 
