ec79f439d7886618f2d799c2422778e3e35747b5
Leticia Amorim <leticiaamorimd@icloud.com>
2022-03-09 22:52:46 +0000

    Adding 'least sold productions' function

--- app.py
+++ app.py
@@ -19,19 +19,25 @@ profitability = pd.read_csv("data/profitability.csv")
 #print(profitability)
 profitability = px.bar(profitability, x="Branch", y = "Profitability", title = "Profitability")
 
-per_hour_data = [{"label": "belfast", "value": "data/per_hour/belf_per_hour.csv"}, 
-                {"label": "east camb", "value": "data/per_hour/eastc_per_hour.csv"}, 
-                {"label": "flins", "value": "data/per_hour/flins_per_hour.csv"}, 
-                {"label": "forest", "value": "data/per_hour/forest_per_hour.csv"}, 
-                {"label": "fylde", "value": "data/per_hour/fylde_per_hour.csv"}, 
-                {"label": "islington", "value": "data/per_hour/islington_per_hour.csv"}, 
-                {"label": "lichfield", "value": "data/per_hour/lichfield_per_hour.csv"}, 
-                {"label": "nyorshire", "value": "data/per_hour/nyorshire_per_hour.csv"}, 
-                {"label": "orkney", "value": "data/per_hour/orkney_per_hour.csv"}]
+per_hour_data = [{"label": "Belfast", "value": "data/per_hour/belf_per_hour.csv"}, 
+                {"label": "East Cambridgeshire", "value": "data/per_hour/eastc_per_hour.csv"}, 
+                {"label": "Flinshire", "value": "data/per_hour/flins_per_hour.csv"}, 
+                {"label": "Forest of Dean", "value": "data/per_hour/forest_per_hour.csv"}, 
+                {"label": "Fylde", "value": "data/per_hour/fylde_per_hour.csv"}, 
+                {"label": "Islington", "value": "data/per_hour/islington_per_hour.csv"}, 
+                {"label": "Lichfield", "value": "data/per_hour/lichfield_per_hour.csv"}, 
+                {"label": "New Yorkshire", "value": "data/per_hour/nyorshire_per_hour.csv"}, 
+                {"label": "Orkney", "value": "data/per_hour/orkney_per_hour.csv"}]
 
-branches = [{"label": "belfast", "value": "data/branches/belf_top5.csv"}]
-
-
+branches = [{"label": "Belfast", "value": "data/branches/belfast.csv"},
+            {"label": "East Cambridgeshire", "value": "data/branches/east_c.csv"},
+            {"label": "Flinshire", "value": "data/branches/flinshire.csv"},
+            {"label": "Forest of Dean", "value": "data/branches/forest.csv"},
+            {"label": "Fylde", "value": "data/branches/fylde.csv"},
+            {"label": "Islington", "value": "data/branches/islington.csv"}, 
+            {"label": "Lichfield", "value": "data/branches/lichfield.csv"}, 
+            {"label": "New Yorkshire", "value": "data/branches/nyorkshire.csv"},
+            {"label": "Orkney", "value": "data/branches/orkney.csv"}]
 
 app = dash.Dash(__name__, title= "Dashboard")
 server = app.server
@@ -40,7 +46,22 @@ app.layout = html.Div([
     html.Div([
         html.H1("Consumer Behaviour"),
     ]),
-      html.Div([
+
+        html.Div([
+        html.H2("Most sold products by each branch"),
+        dcc.Dropdown(options=branches, id="most_least_sold"),
+        html.Div([
+            dcc.Graph(figure= {}, id="belfast")                        
+            ]),
+
+        html.H2("Least sold products by each branch"),
+        dcc.Dropdown(options=branches, id="least_sold"),
+        html.Div([
+            dcc.Graph(figure= {}, id="east")                        
+            ]),
+        ]),
+
+        html.Div([
         html.H2("Quantity sold by all branches"),
         html.Div([
             dcc.Graph(figure= quantity_sold_allbranches_fig, id="quantity_sold_allbranches")                        
@@ -60,14 +81,7 @@ app.layout = html.Div([
             dcc.Graph(figure= profitability, id="profitability")                        
             ]),
         ]),
-                
-        html.Div([
-        html.H2("Quantity sold by Belfast branch"),
-            dcc.Dropdown(options=branches, id="most_least_sold"),
-        html.Div([
-            dcc.Graph(figure= {}, id="belfast")                        
-            ]),
-        ]),
+            
 
         html.Div([
         html.H2("Quantity sold by all branches per hour"),
@@ -79,7 +93,7 @@ app.layout = html.Div([
 
 ])
 
-####Most and least products sold by each branch 
+####Most sold products by each branch 
 @app.callback(
     Output(component_id="belfast", component_property="figure"),
     Input(component_id="most_least_sold", component_property="value")
@@ -89,6 +103,18 @@ def plot_quantity_sold_by_branch(value):
             belfast = pd.read_csv(value)
             belfast = px.bar(belfast.head(), x="products", y = "quantity")
             return belfast
+        return {}
+
+##Least sold products by each branch 
+@app.callback(
+    Output(component_id="east", component_property="figure"),
+    Input(component_id="least_sold", component_property="value")
+)               
+def plot_quantity_sold_by_branch(value):
+        if value is not None: 
+            east = pd.read_csv(value)
+            east = px.bar(east.tail(), x="products", y = "quantity")
+            return east
         return {}
 
 
@@ -101,7 +127,6 @@ def plot_quantity_perhour(value):
     #per hour 
     if value is not None: 
         belfast_perhour = pd.read_csv(value)
-        #figure_one = px.bar(belfast_perhour, y = "hour")
         return px.line(belfast_perhour, y="Sales per hour", x= "hour")
     return {}
 
